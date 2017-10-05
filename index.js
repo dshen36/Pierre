3bcca467265555c395cabff12c2e63cbe1f29922
domenick <dsnook1@bloomberg.net>
2017-10-05 13:41:47 -0400

    [DS] Added payload route for Github webhook

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
-var express = require('express')
-var app = express()
+var express = require('express');
+var app = express();
 
-app.set('port', (process.env.PORT || 5000))
-app.use(express.static(__dirname + '/public'))
+app.set('port', (process.env.PORT || 5000));
+app.use(express.static(__dirname + '/public'));
+
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+
+var payloads = [];
 
 app.get('/', function(request, response) {
-  response.send('Hello World!')
-})
+  response.send(JSON.stringify(payloads));
+});
+
+app.post('/payload', function(request, response){
+  payloads.push(request.body);
+  console.log(request.body);
+  response.send('thanks');
+});
 
 app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
-})
+  console.log("Node app is running at localhost:" + app.get('port'));
+});
