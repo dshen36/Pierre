0fb2e8bda2b472ac296626e8c9413ffe2fd1894b
domenick <dsnook1@bloomberg.net>
2017-10-05 14:02:09 -0400

    [DS] Added payload clear route

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
-var express = require('express')
-var app = express()
+var express = require('express');
+var app = express();
 
-app.set('port', (process.env.PORT || 5000))
-app.use(express.static(__dirname + '/public'))
+app.set('port', (process.env.PORT || 5000));
+app.use(express.static(__dirname + '/public'));
+
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+
+var payloads = [];
 
 app.get('/', function(request, response) {
-  response.send('Hello World!')
-})
+  response.send(JSON.stringify(payloads));
+});
+
+app.get('/clear', function(request, response){
+  payloads = [];
+  response.send(JSON.stringify(payloads));;
+});
+
+app.post('/payload', function(request, response){
+  payloads.push(request.body);
+  console.log(request.body);
+  response.send('added payload');
+});
 
 app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
-})
+  console.log("Node app is running at localhost:" + app.get('port'));
+});
